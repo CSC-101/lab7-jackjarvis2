e2b2a30e99525fdb9a020d302c1c79540c65228f
jackjarvis2 <jajarvis@calpoly.edu>
2024-11-11 15:07:28 -0800

    Update command_line.py

--- command_line.py
+++ command_line.py
@@ -1,1 +1,20 @@
+import sys
+from convert import str_to_float
+def process_arguments(args) -> float:
+    total = 0.0
+    for arg in args:
+        number = str_to_float(arg)
+        if number is not None:
+            total += number
+    return total
+
+
+# Main check
+if __name__ == '__main__':
+    arguments = sys.argv[1:]
+
+    total_sum = process_arguments(arguments)
+
+    print(f"The sum of the valid numbers is: {total_sum}")
+
 
