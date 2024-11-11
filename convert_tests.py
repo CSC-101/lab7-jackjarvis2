6714079e6161d39fea704821a95bee4624220e9f
jackjarvis2 <jajarvis@calpoly.edu>
2024-11-11 15:07:02 -0800

    Update convert_tests.py

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,10 @@
+import unittest
+from convert import str_to_float
+
+class TestStrToFloat(unittest.TestCase):
+
+    def test_valid_float_strings1(self):
+        self.assertEqual(str_to_float("123.45"), 123.45)
+    def test_valid_float_strings2(self):
+        self.assertEqual(str_to_float("abc"), None)
 
